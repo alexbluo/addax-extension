96d7afaec2373ebd5b9d1bd1eb2e879b13df01c7
Alex Luo <alexbluo01@gmail.com>
2023-05-11 11:21:55 -0400

    fix invalid url error on initial parse, add history check

--- extension/scripts/background.ts
+++ extension/scripts/background.ts
@@ -2,36 +2,61 @@
 // also make sure dev server is running...
 // background script output in service worker inspect, content script output in webpage
 
-// check for addax tag in head
-// if present, get site categories and add to chrome.storage
-// auction stuff
-// parse # of ins with addax class
-// request that many ads from advertiser
 // https://medium.com/frontendweb/how-to-add-google-adsense-in-your-nextjs-89e439f74de3
 // set ins content to returned ad through content script
-// page category validation and publisher endpoint not needed
-// publisher setting is in meta tag
 
 const parseAddax = () => {
-  const addax: HTMLMetaElement | null =
-    document.querySelector("meta[name=addax]");
+  const addax = document.querySelector<HTMLMetaElement>("meta[name=addax]");
   const categories = addax?.content.split(" ").map((i) => parseInt(i, 10));
 
   return categories;
 };
 
+// cut off and query string
+const parseTabUrl = (url: string) => {
+  const queryStringIndex =
+    url.indexOf("?") === -1 ? undefined : url.indexOf("?");
+
+  url = url.substring(0, queryStringIndex);
+
+  return url.trim();
+};
+
 // const injectAds = () => {};
 
-const addax = async (tabId: number, changeInfo: chrome.tabs.TabChangeInfo) => {
+const addax = async (
+  tabId: number,
+  changeInfo: chrome.tabs.TabChangeInfo,
+  tab: chrome.tabs.Tab
+) => {
   if (changeInfo.status === "complete") {
+    let err = false;
     const categories = await chrome.scripting
       .executeScript({
         target: { tabId },
         func: parseAddax,
       })
-      .then((data) => data[0].result);
+      .then((data) => data[0].result)
+      .catch(() => {
+        err = true;
+      });
 
-    if (!categories) return;
+    // stop execution if addax is not enabled for the page
+    if (!categories || err) return;
+
+    const parsedUrl = parseTabUrl(tab.url!);
+
+    // check if page has been visited, if so then don't increment interests
+    const historySearch = await chrome.history.search({
+      text: `"${parsedUrl}"`,
+    });
+
+    const visited = historySearch.some(
+      // doesnt match for same path with query strings - massive headache, would need some regex or a genie
+      (item: chrome.history.HistoryItem) => item.url === parsedUrl
+    );
+
+    console.log(visited);
 
     const { interests } = await chrome.storage.local.get({
       // default values for each id if no interests exist yet
@@ -40,9 +65,16 @@ const addax = async (tabId: number, changeInfo: chrome.tabs.TabChangeInfo) => {
         .reduce((acc, cur, i) => ({ ...acc, [i]: 0 }), {}),
     });
 
-    for (const category of categories) {
-      interests[category] += 1;
+    // increment local user interest profile for publishder categories
+    if (!visited) {
+      for (const category of categories) {
+        interests[category] += 1;
+      }
     }
+
+    console.log(interests);
+    // retrieve avertiser list and fetch endpoints
+    // ^ might want to do before incrementing to dissuade publishers from setting to higher conversion categories
 
     await chrome.storage.local.set({ interests });
   }
